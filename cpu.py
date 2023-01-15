aeb3ad705bafbe1d6f0b8595f561711bf670d297
frommwonderland <daniel.illner@outlook.de>
2023-01-15 13:37:25 +0100

    added riscv test as submodule

--- cpu.py
+++ cpu.py
@@ -1,3 +1,23 @@
+import struct
+
+# CPU operates on 32-bit units
+registers = [0] * 32
+# Programm Counter
+PC = 32
+# Stackpointer
+S = 0
+# Processor Flag
+P = [False] * 32
+# 4GB mem
+memory = b'\x00' * 0x1000
+print(type(memory))
+print(memory)
+
+def fetch32(addr):
+    return struct.unpack("I", memory[addr:addr+4])[0]
+
+def step():
+    pass
 # Instruction Fetch
 
 # Instruction Decode
@@ -7,3 +27,6 @@
 # Memory Access
 
 # Writeback
+
+print(struct.unpack("I", b"\x00"*4))
+# print("\x")
