4992f565ca37d7d4e550709c600a4a43fc0155ee
dxlnr <daniel.illner@outlook.de>
2023-03-12 14:30:38 +0100

    added couple of instructions

--- asm.py
+++ asm.py
@@ -13,11 +13,18 @@ class Program(Enum):
     COMMENT = 4
 
 
-# class Instruction(Enum):
-#     ADDI = 0b0010011
-
-
-REG = {"sp": 0b00010, "s0": 0b01000}
+REG = {
+    "sp": 0b00010,
+    "s0": 0b01000,
+    "a0": 10,
+    "a1": 11,
+    "a2": 12,
+    "a3": 13,
+    "a4": 14,
+    "a5": 15,
+    "a6": 16,
+    "a7": 17,
+}
 
 
 def bm(bits: int = 32) -> int:
@@ -28,7 +35,7 @@ def bm(bits: int = 32) -> int:
 def two_compl(val: int, bits: int = 32):
     """Performs sign extension by number of bits given."""
     if val < 0:
-        return bm(12) & val
+        return bm(bits) & val
     else:
         return val
 
@@ -38,7 +45,7 @@ def parse(content: str):
     program = list()
     lines = content.split("\n")
     for line in lines:
-        sl = list(filter(None, re.split(r"\t+|,| ", line)))
+        sl = list(filter(None, re.split(r"\t+|,| |\(|\)", line)))
 
         if sl:
             if re.match("\.+", sl[0]):
@@ -56,8 +63,7 @@ def parse(content: str):
 def encode(ins: list[str]) -> int:
     code = ins[1]
     if code[0] == "addi":
-        value = two_compl(int(code[3]))
-
+        value = two_compl(int(code[3]), 12)
         res = (
             ISA["addi"][0]
             + (REG[code[1]] << 7)
@@ -65,7 +71,40 @@ def encode(ins: list[str]) -> int:
             + (REG[code[2]] << 15)
             + (value << 20)
         )
-        print("Ins : ", hex(res))
+
+    if code[0] == "sb" or code[0] == "sh" or code[0] == "sw":
+        value = two_compl(int(code[2]), 32)
+        res = (
+            ISA[code[0]][0]
+            + ((value & bm(5)) << 7)
+            + (ISA[code[0]][1] << 12)
+            + (REG[code[3]] << 15)
+            + (REG[code[1]] << 20)
+            + (((value >> 5) & bm(7)) << 25)
+        )
+
+    if code[0] == "li":
+        value = two_compl(int(code[2]), 12)
+        res = (
+            ISA["li"][0]
+            + (REG[code[1]] << 7)
+            + (ISA["li"][1] << 12)
+            + (ISA["li"][2] << 15)
+            + (value << 20)
+        )
+
+    if code[0] == "lb" or code[0] == "lh" or code[0] == "lw":
+        value = two_compl(int(code[2]), 12)
+        res = (
+            ISA[code[0]][0]
+            + (REG[code[1]] << 7)
+            + (ISA[code[0]][1] << 12)
+            + (REG[code[3]] << 15)
+            + (value << 20)
+        )
+
+    print("Ins : ", hex(res))
+    return res
 
 
 def main():
