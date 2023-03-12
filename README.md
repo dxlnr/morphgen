70a47dca98f4949d5d6cfc856297547b69fdbcfa
dxlnr <daniel.illner@outlook.de>
2023-03-12 23:38:26 +0100

    readme & upd asm

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Implementing a Processor using the [RISC-V](https://riscv.org/wp-content/uploads/2017/05/riscv-spec-v2.2.pdf) instruction set. 
 
-### Getting Started
+### CPU
 
 For testing the **cpu**, clone and build the `riscv-tests`. The tests can be found in the modules/ folder. Please follow along the installation [guide](modules/README.md).
 
@@ -13,24 +13,18 @@ python cpu.py
 
 ### Assembler 
 
-Implementation steps  
-- replace '...' occurances with ASCII code 
-- delete ; comments 
-- replace, with space 
-- process and cut preprocessor commands like #org 
-- cut & store <label>: definitions in a <label> -> linenumber dict 
-- split line into a list where each list element represents a byte 
-- replace mnemonics with opcodes 
-- replace 16-bit words with LSB MSB 
+Run the Assembler via:
+```bash 
+python asm.py example/minimal.s
+``` 
+Currently under development. 
+Todo:
+- Linking
+- Implementing all the instructions.
 
-- insert a place holder after each label reference for the address MSB
-- calculate the address of each line 
-- update dict from <label> -> linenumber to <label> address 
 
-- replace 'label reference + placeholder' with address 'MSB LSB'
-- check if remaining elements are numeric if not display error message
-
-Get the *desired output* by running 
+The *desired output* should be somewhat similiar to the following, although this is 
+64-bit based. Obtain it by running:
 ```bash 
 xxd -a modules/riscv-tests/isa/rv32ui-p-add 
 ```
@@ -50,11 +44,6 @@ xxd -a modules/riscv-tests/isa/rv32ui-p-add
 *
 00001000: 6f00 8004 732f 2034 930f 8000 6308 ff03  o...s/ 4....c...
 00001010: 930f 9000 6304 ff03 930f b000 6300 ff03  ....c.......c...
-00001020: 130f 0000 6304 0f00 6700 0f00 732f 2034  ....c...g...s/ 4
-00001030: 6354 0f00 6f00 4000 93e1 9153 171f 0000  cT..o.@....S....
-00001040: 2322 3ffc 6ff0 9fff 9300 0000 1301 0000  #"?.o...........
-00001050: 9301 0000 1302 0000 9302 0000 1303 0000  ................
-00001060: 9303 0000 1304 0000 9304 0000 1305 0000  ................
 ```
 
 
--- asm.py
+++ asm.py
@@ -30,6 +30,7 @@ def parse(content: str):
     """Reads the assmbly code and returns list of tokenized symbols."""
     program = list()
     lines = content.split("\n")
+    pc = 0
     for line in lines:
         sl = list(filter(None, re.split(r"\t+|,| |\(|\)", line)))
 
@@ -37,23 +38,27 @@ def parse(content: str):
             if re.match("\.+", sl[0]):
                 program.append((Program.DIRECTIVE, sl))
             elif re.match("\w+(?: \w+)*:", sl[0]):
-                program.append((Program.LABEL, sl))
+                program.append((Program.LABEL, sl, pc))
             elif re.match("\#+", sl[0]):
                 program.append((Program.COMMENT, sl))
             else:
-                program.append((Program.INSTRUCTION, sl))
+                program.append((Program.INSTRUCTION, sl, pc))
+                pc += 4
 
     return program
 
 
-def encode(ins: list[str]) -> int:
+def encode(enc: list[int], ins: list[str]) -> int:
     code = ins[1]
     if code[0] == "addi" or code[0] == "mv":
         if code[0] == "mv":
             value = 0
         else:
-            value = two_compl(int(code[3]), 12)
-        res = (
+            try:
+                value = two_compl(int(code[3]), 12)
+            except:
+                value = 0
+        enc.append(
             ISA[code[0]][0]
             + (REG[code[1]] << 7)
             + (ISA[code[0]][1] << 12)
@@ -63,7 +68,7 @@ def encode(ins: list[str]) -> int:
 
     if code[0] == "sb" or code[0] == "sh" or code[0] == "sw":
         value = two_compl(int(code[2]), 32)
-        res = (
+        enc.append(
             ISA[code[0]][0]
             + ((value & bm(5)) << 7)
             + (ISA[code[0]][1] << 12)
@@ -74,7 +79,7 @@ def encode(ins: list[str]) -> int:
 
     if code[0] == "li":
         value = two_compl(int(code[2]), 12)
-        res = (
+        enc.append(
             ISA[code[0]][0]
             + (REG[code[1]] << 7)
             + (ISA[code[0]][1] << 12)
@@ -84,7 +89,7 @@ def encode(ins: list[str]) -> int:
 
     if code[0] == "lb" or code[0] == "lh" or code[0] == "lw":
         value = two_compl(int(code[2]), 12)
-        res = (
+        enc.append(
             ISA[code[0]][0]
             + (REG[code[1]] << 7)
             + (ISA[code[0]][1] << 12)
@@ -93,7 +98,7 @@ def encode(ins: list[str]) -> int:
         )
 
     if code[0] == "add" or code[0] == "sub":
-        res = (
+        enc.append(
             ISA[code[0]][0]
             + (REG[code[1]] << 7)
             + (ISA[code[0]][1] << 12)
@@ -102,9 +107,9 @@ def encode(ins: list[str]) -> int:
             + (ISA[code[0]][2] << 25)
         )
 
-    if code[0] == "jalr": 
+    if code[0] == "jalr":
         value = two_compl(int(code[2]), 12)
-        res = (
+        enc.append(
             ISA[code[0]][0]
             + (REG[code[1]] << 7)
             + (ISA[code[0]][1] << 12)
@@ -112,14 +117,19 @@ def encode(ins: list[str]) -> int:
             + (value << 20)
         )
 
-    if code[0] == "jr":
-        res = ISA[code[0]][0] + (REG[code[1]] << 7) + (ISA[code[0]][1] << 12)
- 
-    if code[0] == "call":
-        # res = ISA[code[0]][0] + 
-        pass
+    if code[0] == "lui":
+        enc.append(ISA[code[0]][0] + (REG[code[1]] << 7))
 
-    return res
+    if code[0] == "jr":
+        enc.append(ISA[code[0]][0] + (REG[code[1]] << 7) + (ISA[code[0]][1] << 12))
+
+    if code[0] == "call":
+        enc.append(ISA["auipc"][0] + (REG["ra"] << 7))
+        enc.append(
+            ISA["jalr"][0] + (REG["ra"] << 7) + (ISA["jalr"][1] << 12) + (1 << 15)
+        )
+
+    return enc
 
 
 def main():
@@ -132,12 +142,15 @@ def main():
     content = open(x, "r").read()
 
     t = parse(content)
+    enc = list()
     for line in t:
-        print(line)
         if line[0] == Program.INSTRUCTION:
-            res = encode(line)
-            print("Ins : ", hex(res))
-    print("")
+            enc = encode(enc, line)
+
+    # Print results.
+    print("    Instructions:")
+    for e in enc:
+        print(f"\t%08x " % e)
 
 
 if __name__ == "__main__":
