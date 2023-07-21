431ecc2faa0fd34639b7345b44ecc59146a0fdbc
dxlnr <daniel.illner@outlook.de>
2023-07-21 02:10:38 +0200

    upd

--- cpu/arm_cpu.v
+++ cpu/arm_cpu.v
@@ -18,9 +18,9 @@ module processor
 
     wire [11:0] imm12 = ins[11:0];
     wire [3:0] rd = ins[15:12];
-    wire [3:0] op1 = ins[19:16];
+    wire [3:0] rs = ins[19:16];
     wire s = ins[20];
-    wire [7:0] func = ins[27:21];
+    wire [7:0] ops = ins[27:21];
     wire [3:0] cond = ins[31:28];
  
     always @(posedge clk) begin
@@ -28,7 +28,66 @@ module processor
 
         ins <= r.mem[pc];
         $display("ins: %h", ins, ", pc: %h", pc);
-        $display("%b", cond, " %b", func, " %b", s, " %b", op1, " %b", rd, " %b", imm12);
+        $display("%b", cond, " %b", ops, " %b", s, " %b", rs, " %b", rd, " %b", imm12);
         $display("\n");
+
+        case (ops)   
+            8'b0000000: begin
+            end
+            8'b0000001: begin
+            end
+            8'b0000010: begin
+            end
+            8'b0000011: begin
+            end
+            8'b0000100: begin
+            end
+            8'b0000101: begin
+            end
+            8'b0000110: begin
+            end
+            8'b0000111: begin
+            end
+            8'b0001000: begin
+            end
+            8'b0001001: begin
+            end
+            8'b0001010: begin
+            end
+            8'b0001011: begin
+            end
+            8'b0001100: begin
+            end
+            8'b0001101: begin
+            end
+            8'b0001110: begin
+            end
+            8'b0001111: begin
+            end
+            8'b0010000: begin
+            end
+            8'b0010001: begin
+            end
+            8'b0010010: begin
+            end
+            8'b0010011: begin
+            end
+            8'b0010100: begin
+            end
+            8'b0010101: begin
+            end
+            8'b0010110: begin
+            end
+            8'b0010111: begin
+            end
+
+            8'b0101001: begin
+                // STR (immediat):
+                // offset_addr = if add (ins[23]) then (R[n] + imm32) else (R[n] - imm32);
+                // address = if index then offset_addr else R[n];
+                // MemU[address,4] = if t == 15 then PCStoreValue() else R[t];
+                // if wback then R[n] = offset_addr;
+            end
+        endcase
     end
 endmodule
